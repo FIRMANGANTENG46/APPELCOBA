e07babbd64f64b0f1c1b72e4ad3143e2e7768a4c
FIRMANGANTENG46 <firmansyah18@mhs.akba.ac.id>
2022-09-17 23:32:21 +0800

    api

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
-use App\Http\Controllers\API\ProductController;
-use App\Http\Controllers\API\TransactionController;
-use App\Http\Controllers\API\ProductCategoryController;
+use App\Http\Controllers\API\PertemuanController;
+use App\Http\Controllers\API\MateriController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,13 +23,11 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('user', [UserController::class, 'updateProfile']);
     Route::post('logout', [UserController::class, 'logout']);
 
-    Route::get('transactions', [TransactionController::class, 'all']);
-    Route::post('checkout', [TransactionController::class, 'checkout']);
 });
 
 
-Route::get('products', [ProductController::class, 'all']);
-Route::get('categories', [ProductCategoryController::class, 'all']);
+Route::get('pertemuans', [PertemuanController::class, 'all']);
+Route::get('materis', [MateriController::class, 'all']);
 
 Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
