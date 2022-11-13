9078c8645390b78e78e76aa1f959fa53c6761426
FIRMANGANTENG46 <firmansyah18@mhs.akba.ac.id>
2022-11-13 15:04:05 +0800

    jjjj

--- app/Http/Controllers/API/PertemuanController.php
+++ app/Http/Controllers/API/PertemuanController.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Models\Pertemuan;
-use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Models\Pertemuan;
+use Illuminate\Http\Request;
 
 class PertemuanController extends Controller
 {
     public function all(Request $request)
     {
         $id = $request->input('id');
-        $limit = $request->input('limit', 6);
+        $limit = $request->input('limit', 1000);
         $name = $request->input('name');
         $description = $request->input('description');
         $tags = $request->input('tags');
@@ -21,46 +21,55 @@ class PertemuanController extends Controller
         $price_from = $request->input('price_from');
         $price_to = $request->input('price_to');
 
-        if($id)
-        {
-            $pertemuan = Pertemuan::with(['materi','galleries'])->find($id);
+        if ($id) {
+            $pertemuan = Pertemuan::with(['materi', 'galleries'])->find($id);
 
-            if($pertemuan)
+            if ($pertemuan) {
                 return ResponseFormatter::success(
                     $pertemuan,
                     'Data produk berhasil diambil'
                 );
-            else
+            } else {
                 return ResponseFormatter::error(
                     null,
                     'Data produk tidak ada',
                     404
                 );
+            }
+
         }
 
-        $pertemuan = Pertemuan::with(['materi','galleries']);
+        $pertemuan = Pertemuan::with(['materi', 'galleries']);
 
-        if($name)
+        if ($name) {
             $pertemuan->where('name', 'like', '%' . $name . '%');
+        }
 
-        if($description)
+        if ($description) {
             $pertemuan->where('description', 'like', '%' . $description . '%');
+        }
 
-        if($tags)
+        if ($tags) {
             $pertemuan->where('tags', 'like', '%' . $tags . '%');
+        }
 
-        if($price_from)
+        if ($price_from) {
             $pertemuan->where('price', '>=', $price_from);
+        }
 
-        if($price_to)
+        if ($price_to) {
             $pertemuan->where('price', '<=', $price_to);
+        }
 
-        if($materis)
+        if ($materis) {
             $pertemuan->where('categories_id', $materis);
+        }
 
         return ResponseFormatter::success(
             $pertemuan->paginate($limit),
             'Data list produk berhasil diambil'
         );
     }
+
+//limit dihapus krn membatasi
 }
